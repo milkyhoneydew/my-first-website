afaad268436e280fda30110a5313317e032e5281
Tanya Prado <tanyaprado@gmail.com>
2022-10-30 15:58:28 -0700

    add default and large font buttons and events

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,23 @@
 window.onload = function() {
   let lightBtn = document.getElementById("lightBtn");
   lightBtn.onclick = function() {
-    document.querySelector("body").style.backgroundColor = "neongreen";
+    document.querySelector("body").style.backgroundColor = "pink";
     document.querySelector("body").style.color = "black";
   };
 
-  let darkBtn = document.getElementById("drkBtn");
+  let darkBtn = document.getElementById("darkBtn");
   darkBtn.onclick = function() {
     document.querySelector("body").style.backgroundColor = "black";
     document.querySelector("body").style.color = "white";
+  };
+
+  let defaultFontBtn = document.getElementById("defaultFontBtn");
+  defaultFontBtn.onclick = function() {
+    document.querySelector("body").style.fontSize = "initial";
+  };
+
+  let bigFontBtn = document.getElementById("bigFontBtn");
+  bigFontBtn.onclick = function() {
+    document.querySelector("body").style.fontSize = "xx-large";
   };
 };
